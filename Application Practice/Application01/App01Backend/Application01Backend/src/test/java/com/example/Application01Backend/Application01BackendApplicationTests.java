647e4838a089e9e60213114155a1a7c3f7702615
Sharwan Jung Kunwar <sharwankunwar07@gmail.com>
2026-01-10 09:46:32 +0545

    added day01 application practice

---
+++ Application Practice/Application01/App01Backend/Application01Backend/src/test/java/com/example/Application01Backend/Application01BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Application01Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application01BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
