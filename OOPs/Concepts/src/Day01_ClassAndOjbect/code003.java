f5c875556dc5aa8037d89b11f5db883cc8dda05f
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-01-24 22:34:56 +0545

    updated

---
+++ OOPs/Concepts/src/Day01_ClassAndOjbect/code003.java
@@ -0,0 +1,30 @@
+
+
+public class code003 {
+    public static void main(String[] args) {
+        String str01 = "abzbaa";
+        String str02 = "abbaaz";
+
+        if (str01.length() != str02.length()) {
+            System.out.println("The lengths are not equal");
+            return;
+        }
+        
+        int[] freq = new int[256]; // ASCII character count
+
+        for (int i = 0; i < str01.length(); i++) {
+            freq[str01.charAt(i)]++;   // increment for str01
+            freq[str02.charAt(i)]--;   // decrement for str02
+        }
+
+        boolean isAnagram = true;
+        for (int count : freq) {
+            if (count != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+
+        System.out.println(isAnagram ? "Anagram" : "Not Anagram");
+    }
+}
