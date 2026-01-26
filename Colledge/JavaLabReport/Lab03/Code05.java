88b26aac5449c289904c6bd0cc988d9e17c9f8a5
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-01-26 20:27:38 +0545

    updaed

---
+++ Colledge/JavaLabReport/Lab03/Code05.java
@@ -0,0 +1,45 @@
+package JavaLab.Lab03;
+
+import java.util.Scanner;
+
+public class Code05 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Chose the operation [ +, -, x, /, % ] : ");
+        char operation = input.next().charAt(0);
+        System.out.print("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        if(num1 < 1){
+            System.out.println("not allowed");
+        }else  if(num2 < 1){
+            System.out.println("not allowed");
+        }
+
+        switch (operation) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                System.out.println(num1 / num2);
+                break;
+            case '%':
+                System.out.println(num1 % num2);
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+
+
+    }
+}
