5b35e0b287d3db7f15ee9ec73711939a9611107d
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-02-20 09:02:58 +0545

    updated dashboard

--- Personal Projects/Focus Planner/FocusPlanner/src/pages/Dashboard.jsx
+++ Personal Projects/Focus Planner/FocusPlanner/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SparklesPreview } from '../components/ui/SparklesPreview'
+import { Card, Button } from 'antd'
 
 function Dashboard() {
   return (
@@ -11,9 +12,16 @@ function Dashboard() {
               <SparklesPreview/>
             </div>
 
-            <div className='bg-red-400 h-full flex gap-1'>
-              <div className='bg-yellow-400 w-[300px] h-[200px]'>
-                box
+            <div className=' h-full flex flex-col gap-5'>
+              
+              {/* top div  */}
+              <div className='w-full h-[80%] grid grid-cols-3 items-start gap-5'>
+                <Card className='bg-gray-50! h-full! rounded shadow-sm'></Card>
+                <Card className='bg-gray-50! h-full! rounded shadow-sm'></Card>
+                <Card className='bg-gray-50! h-full! rounded shadow-sm'></Card>
+              </div>
+              <div className='bg-gray-50 shadow-sm h-[20%] grid grid-cols-5 items-center gap-5 rounded px-5'>
+                
               </div>
             </div>
 
