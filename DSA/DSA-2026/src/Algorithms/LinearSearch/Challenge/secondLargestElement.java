36a20aef554fdf152fdef8bd58c29b195f9562c2
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-02-27 22:59:24 +0545

    updaed

---
+++ DSA/DSA-2026/src/Algorithms/LinearSearch/Challenge/secondLargestElement.java
@@ -0,0 +1,29 @@
+package Algorithms.LinearSearch.Challenge;
+
+/*
+    🔹 Problem: Find Second Largest Element (Using Linear Search Only)
+
+        You are given an integer array:
+
+        int[] numbers = {12, 45, 7, 89, 45, 23, 90, 7, 88};
+        🎯 Your Tasks:
+
+        Find the largest element
+
+        Find the second largest element
+
+        If second largest does not exist (like all elements same), return -1
+
+        You must use Linear Search only
+
+        ❌ No sorting
+        ❌ No built-in functions
+        ❌ No Collections
+        ✔ Only loops and conditions
+ */
+
+public class secondLargestElement {
+    static void main(String[] args) {
+
+    }
+}
