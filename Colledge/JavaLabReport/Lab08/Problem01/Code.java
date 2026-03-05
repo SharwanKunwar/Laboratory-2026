fab4b1d32842d66b90480b417a6aa4a662708435
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-05 09:39:33 +0545

    added lab08 of java

---
+++ Colledge/JavaLabReport/Lab08/Problem01/Code.java
@@ -0,0 +1,9 @@
+package JavaLab.Lab08.Problem01;
+
+// Write a java program to demonstrate ArithmeticException, ArrayIndexOutOfBoundsException, and NumberFormateException
+
+public class Code {
+    static void main(String[] args) {
+
+    }
+}
