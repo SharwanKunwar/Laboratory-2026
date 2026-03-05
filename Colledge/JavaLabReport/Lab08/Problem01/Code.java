0002d2f1c0969ba68094da3d401956fc0e4848f2
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-05 09:50:55 +0545

    added question01

--- Colledge/JavaLabReport/Lab08/Problem01/Code.java
+++ Colledge/JavaLabReport/Lab08/Problem01/Code.java
@@ -2,8 +2,39 @@ package JavaLab.Lab08.Problem01;
 
 // Write a java program to demonstrate ArithmeticException, ArrayIndexOutOfBoundsException, and NumberFormateException
 
+import java.util.Scanner;
+
 public class Code {
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+
+
+        int [] numbers = {1,2,3};
+
+        System.out.println("-------------------- ArithmeticException --------------------");
+        int a = 5,b=0;
+        try{
+            int div = a/b;
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero");
+        }
+
+
+        System.out.println("-------------------- ArrayIndexOutOfBoundsException --------------------");
+        try{
+            System.out.println(numbers[3]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array Index Out Of Bounds Error aayo");
+        }
+
+        System.out.println("-------------------- NumberFormateException --------------------");
+        System.out.println("Enter your option: ");
+
+        try{
+            int op = in.nextInt();
+        }catch (NumberFormatException e){
+            System.out.println("Number Format Error aayo");
+        }
 
     }
 }
