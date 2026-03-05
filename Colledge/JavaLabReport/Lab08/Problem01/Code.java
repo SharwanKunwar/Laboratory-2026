a6de97c0f5b020f1d336a6465a566b8d2b9bff1f
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-05 10:05:14 +0545

    added tow qeusitons

--- Colledge/JavaLabReport/Lab08/Problem01/Code.java
+++ Colledge/JavaLabReport/Lab08/Problem01/Code.java
@@ -27,12 +27,13 @@ public class Code {
             System.out.println("Array Index Out Of Bounds Error aayo");
         }
 
-        System.out.println("-------------------- NumberFormateException --------------------");
-        System.out.println("Enter your option: ");
+        System.out.println("-------------------- NumberFormatException --------------------");
 
         try{
-            int op = in.nextInt();
-        }catch (NumberFormatException e){
+            String num = "abc";
+            int value = Integer.parseInt(num);
+        }
+        catch(NumberFormatException e){
             System.out.println("Number Format Error aayo");
         }
 
---
+++ Colledge/JavaLabReport/Lab08/Problem02/Code.java
@@ -0,0 +1,29 @@
+package JavaLab.Lab08.Problem02;
+
+// Write a java program that will read balance and withdraw amount from keyboard and display the remaining
+// balance on screen if the balance is greater than withdraw amount otherwise throw an exception with appropriate message.
+
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+
+        int balance = 10000;
+        System.out.println("Your balance is : "+balance);
+        System.out.println("-----------------------------------");
+        System.out.print("Enter your withdrawal amount : ");
+        int withdraw = in.nextInt();
+
+
+        if(withdraw>balance){
+            throw new Exception("Not enough balance");
+        }else {
+            System.out.println("Your balance is : "+balance);
+            System.out.println("Your withdrawal amount is : "+withdraw);
+            int finalBalance = balance - withdraw;
+            System.out.println("Your balance is : "+finalBalance);
+        }
+    }
+}
