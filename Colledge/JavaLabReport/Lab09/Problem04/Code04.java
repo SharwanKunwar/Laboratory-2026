de57f594467fe5e2f91b83cf0d82d23cbccebba1
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-11 15:22:57 +0545

    added autoboxing and unboxing using wrapper class

---
+++ Colledge/JavaLabReport/Lab09/Problem04/Code04.java
@@ -0,0 +1,22 @@
+package JavaLab.Lab09.Problem04;
+
+// Write a java program to perform autoboxing and unboxing using wrapper class.
+
+public class Code04 {
+    public static void main(String[] args) {
+
+        // Autoboxing: converting primitive to wrapper object
+        int num = 10;
+        Integer obj = num;   // autoboxing
+
+        System.out.println("Primitive value: " + num);
+        System.out.println("Wrapper object (Autoboxing): " + obj);
+
+        // Unboxing: converting wrapper object to primitive
+        Integer obj2 = 20;
+        int num2 = obj2;   // unboxing
+
+        System.out.println("Wrapper object: " + obj2);
+        System.out.println("Primitive value (Unboxing): " + num2);
+    }
+}
