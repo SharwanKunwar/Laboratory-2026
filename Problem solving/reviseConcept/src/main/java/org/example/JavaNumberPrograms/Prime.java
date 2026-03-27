c315a19066e8adabf8d04d86e42fb7c4639632d1
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-27 14:24:32 +0545

    added

--- Problem solving/reviseConcept/src/main/java/org/example/JavaNumberPrograms/Prime.java
+++ Problem solving/reviseConcept/src/main/java/org/example/JavaNumberPrograms/Prime.java
@@ -1,4 +1,21 @@
 package org.example.JavaNumberPrograms;
 
 public class Prime {
+    public static void main(String[] args) {
+        int num = 7;
+        boolean flag = true;
+
+        if(num<1) return;
+
+        for(int i=2; i*i<=num; i++){
+            if(num%2 == 0){
+                flag = false;
+            }
+
+        }
+
+        if(flag) System.out.println("Prime Number");
+        else System.out.println("Not Prime Number");
+    }
+
 }
